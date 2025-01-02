12c40d1b2e11431495bac9cc81de3409c8a7c94a
torsteinelv <torstein@thorsland.net>
2025-01-02 18:04:10 +0100

    Update vllm_engine.py

--- ray-serve/vllm_engine.py
+++ ray-serve/vllm_engine.py
@@ -1,125 +1,125 @@
+import os
+
+from typing import Dict, Optional, List
+import logging
+
+from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import StreamingResponse, JSONResponse
+
+from ray import serve
+
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
-from vllm.sampling_params import SamplingParams
-from starlette.responses import Response
-from starlette.requests import Request
-from http import HTTPStatus
-
-from fastapi import FastAPI, HTTPException
-import logging
-import uuid
-
-import nest_asyncio
-from ray import serve
-from ray.serve import Application
-from typing import Optional, Literal, List, Dict
-from pydantic import BaseModel
-
+from vllm.entrypoints.openai.cli_args import make_arg_parser
+from vllm.entrypoints.openai.protocol import (
+    ChatCompletionRequest,
+    ChatCompletionResponse,
+    ErrorResponse,
+)
+from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
+from vllm.entrypoints.openai.serving_engine import LoRAModulePath
+from vllm.utils import FlexibleArgumentParser
 
 logger = logging.getLogger("ray.serve")
 
 app = FastAPI()
 
-class Message(BaseModel):
-    role: Literal["system", "assistant", "user"]
-    content: str
 
-    def __str__(self):
-        return self.content
-
-class GenerateRequest(BaseModel):
-    max_tokens: Optional[int] = 128
-    temperature: Optional[float] = 0.7
-    prompt: Optional[str]
-    messages: Optional[List[Message]]
-
-class GenerateResponse(BaseModel):
-    output: Optional[str]
-    finish_reason: Optional[str]
-    prompt: Optional[str]
-
-def _prepare_engine_args(num_gpus: int, tensor_parallel_size: int, model: str, dtype: str, trust_remote_code: bool):
-    engine_args = AsyncEngineArgs(
-        model=model,
-        trust_remote_code=trust_remote_code,
-        dtype=dtype,
-        tensor_parallel_size=tensor_parallel_size,  # Support tensor parallelism
-    )
-    return engine_args
-
-@serve.deployment(name='VLLMInference', 
-                  num_replicas=1, 
-                  max_concurrent_queries=256,
-                  ray_actor_options={"num_gpus": 1.0}  # Set this as per GPU requirements
-                 )
+@serve.deployment(name="VLLMDeployment")
 @serve.ingress(app)
-class VLLMInference:
-    def __init__(self, model: str, num_gpus: int, tensor_parallel_size: int, **kwargs):
-        super().__init__(app)
-        self.args = _prepare_engine_args(num_gpus, tensor_parallel_size, model, dtype="float16", trust_remote_code=True)
-        self.engine = AsyncLLMEngine.from_engine_args(self.args)
-        self.tokenizer = self._prepare_tokenizer()
+class VLLMDeployment:
+    def __init__(
+        self,
+        engine_args: AsyncEngineArgs,
+        response_role: str,
+        lora_modules: Optional[List[LoRAModulePath]] = None,
+        chat_template: Optional[str] = None,
+    ):
+        logger.info(f"Starting with engine args: {engine_args}")
+        self.openai_serving_chat = None
+        self.engine_args = engine_args
+        self.response_role = response_role
+        self.lora_modules = lora_modules
+        self.chat_template = chat_template
+        self.engine = AsyncLLMEngine.from_engine_args(engine_args)
 
-    def _prepare_tokenizer(self):
-        from transformers import AutoTokenizer
-        tokenizer = AutoTokenizer.from_pretrained(self.args.model, trust_remote_code=True)
-        return tokenizer
+    @app.post("/v1/chat/completions")
+    async def create_chat_completion(
+        self, request: ChatCompletionRequest, raw_request: Request
+    ):
+        """OpenAI-compatible HTTP endpoint.
 
-    @app.post("/generate", response_model=GenerateResponse)
-    async def generate_text(self, request: GenerateRequest, raw_request: Request) -> GenerateResponse:
-        logging.info(f"Received request: {request}")
-        try:
-            generation_args = request.dict(exclude={'prompt', 'messages'})
-            if generation_args is None:
-                generation_args = {
-                    "max_tokens": 500,
-                    "temperature": 0.1,
-                }
-            
-            if request.prompt:
-                prompt = request.prompt
-            elif request.messages:
-                prompt = self.tokenizer.apply_chat_template(
-                    request.messages,
-                    tokenize=False,
-                    add_generation_prompt=True
-                )
+        API reference:
+            - https://docs.vllm.ai/en/latest/serving/openai_compatible_server.html
+        """
+        if not self.openai_serving_chat:
+            model_config = await self.engine.get_model_config()
+            # Determine the name of the served model for the OpenAI client.
+            if self.engine_args.served_model_name is not None:
+                served_model_names = self.engine_args.served_model_name
             else:
-                raise ValueError("Prompt or Messages is required")
+                served_model_names = [self.engine_args.model]
+            self.openai_serving_chat = OpenAIServingChat(
+                self.engine,
+                model_config,
+                served_model_names=served_model_names,
+                response_role=self.response_role,
+                lora_modules=self.lora_modules,
+                chat_template=self.chat_template,
+                prompt_adapters=None,
+                request_logger=None,
+            )
+        logger.info(f"Request: {request}")
+        generator = await self.openai_serving_chat.create_chat_completion(
+            request, raw_request
+        )
+        if isinstance(generator, ErrorResponse):
+            return JSONResponse(
+                content=generator.model_dump(), status_code=generator.code
+            )
+        if request.stream:
+            return StreamingResponse(content=generator, media_type="text/event-stream")
+        else:
+            assert isinstance(generator, ChatCompletionResponse)
+            return JSONResponse(content=generator.model_dump())
 
-            sampling_params = SamplingParams(**generation_args)
-            request_id = self._next_request_id()
-            results_generator = self.engine.generate(prompt, sampling_params, request_id)
 
-            final_result = None
-            async for result in results_generator:
-                if await raw_request.is_disconnected():
-                    await self.engine.abort(request_id)
-                    return GenerateResponse()
-                final_result = result  # Store the last result
-            if final_result:
-                return GenerateResponse(output=final_result.outputs[0].text,
-                                        finish_reason=final_result.outputs[0].finish_reason,
-                                        prompt=final_result.prompt)
-            else:
-                raise ValueError("No results found")
-        except ValueError as e:
-            raise HTTPException(HTTPStatus.BAD_REQUEST, str(e))
-        except Exception as e:
-            logger.error('Error in generate()', exc_info=1)
-            raise HTTPException(HTTPStatus.INTERNAL_SERVER_ERROR, 'Server error')
+def parse_vllm_args(cli_args: Dict[str, str]):
+    """Parses vLLM args based on CLI inputs.
 
-    @staticmethod
-    def _next_request_id():
-        return str(uuid.uuid1().hex)
+    Currently uses argparse because vLLM doesn't expose Python models for all of the
+    config options we want to support.
+    """
+    parser = FlexibleArgumentParser(description="vLLM CLI")
+    parser = make_arg_parser(parser)
+    arg_strings = []
+    for key, value in cli_args.items():
+        arg_strings.extend([f"--{key}", str(value)])
+    logger.info(arg_strings)
+    parsed_args = parser.parse_args(args=arg_strings)
+    return parsed_args
 
-    async def _abort_request(self, request_id) -> None:
-        await self.engine.abort(request_id)
 
-    @app.get("/health")
-    async def health(self) -> Response:
-        """Health check."""
-        return Response(status_code=200)
+def build_app(cli_args: Dict[str, str]) -> serve.Application:
+    """Builds the Serve app based on CLI arguments.
 
-def deployment_llm(args: Dict[str, str]) -> Application:
-    return VLLMInference.bind(**args)
+    See https://docs.vllm.ai/en/latest/serving/openai_compatible_server.html#command-line-arguments-for-the-server
+    for the complete set of arguments.
+
+    Supported engine arguments: https://docs.vllm.ai/en/latest/models/engine_args.html.
+    """  # noqa: E501
+    parsed_args = parse_vllm_args(cli_args)
+    engine_args = AsyncEngineArgs.from_cli_args(parsed_args)
+    engine_args.worker_use_ray = True
+
+    return VLLMDeployment.bind(
+        engine_args,
+        parsed_args.response_role,
+        parsed_args.lora_modules,
+        parsed_args.chat_template,
+    )
+
+
+model = build_app(
+    {"model": os.environ['MODEL_ID'], "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']})
